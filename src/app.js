7bd8ab47616540245a403c784d2f7ec0e02ad28f
João Paulo <musicjoaopaulo@gmail.com>
2018-05-08 00:29:52 -0300

    Add files

---
+++ src/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import React from 'react'
+
+const App = React.createClass({
+  render: function () {
+    return <h2>Aplicação</h2>
+  }
+})
+
+// var Title = React.createClass({
+//   render: function() {
+//     return <h1>Título2</h1>
+//   }
+// })
+
+export default App
